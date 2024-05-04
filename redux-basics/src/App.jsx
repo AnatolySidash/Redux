7f0a5905b101991ff02d59cdb25fda63ef71103d
Anatoly SIdash <sidash85@yandex.ru>
2024-05-04 12:31:25 +0300

    feat: add async data request by fetch using redux thunk

--- redux-basics/src/App.jsx
+++ redux-basics/src/App.jsx
@@ -1,11 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { addCustomerAction, deleteCustomerAction } from './store/customerReducer';
+import { getCustomers } from './asyncActions/customers';
 
 function App() {
   const dispatch = useDispatch();
   const cash = useSelector(state => state.cash.cash);
   const customers = useSelector(state => state.customers.customers);
-  console.log(cash);
 
   const addMoney = (cash) => {
     dispatch({ type: "ADD_CASH", payload: cash })
@@ -38,10 +38,11 @@ function App() {
           <button onClick={() => addMoney(Number(prompt()))} className='main__add-money'>Положить деньги</button>
           <button onClick={() => getMoney(Number(prompt()))} className='main__add-money blue'>Снять деньги</button>
           <button onClick={() => addCustomer(prompt())} className='main__add-money'>Добавить клиента</button>
+          <button onClick={() => dispatch(getCustomers())} className='main__add-money'>Получить клиентов</button>
           <div>
             {
               customers.length > 0 ?
-                <div>
+                <div className='main__list'>
                   {customers.map(customer => <div className='main__customer' onClick={() => deleteCustomer(customer)} key={customer.id}>{customer.name}</div>
                   )}
                 </div>
--- redux-basics/src/store/customerReducer.js
+++ redux-basics/src/store/customerReducer.js
@@ -3,10 +3,13 @@ const defaultState = {
 }
 
 const ADD_CUSTOMER = "ADD_CUSTOMER";
+const ADD_MANY_CUSTOMERS = "ADD_MANY_CUSTOMERS";
 const DELETE_CUSTOMER = "DELETE_CUSTOMER";
 
 export const customerReducer = (state = defaultState, action) => {
    switch (action.type) {
+      case ADD_MANY_CUSTOMERS:
+         return { ...state, customers: [...state.customers, ...action.payload] }
       case ADD_CUSTOMER:
          return { ...state, customers: [...state.customers, action.payload] }
       case DELETE_CUSTOMER:
@@ -19,4 +22,5 @@ export const customerReducer = (state = defaultState, action) => {
 
 
 export const addCustomerAction = (payload) => ({ type: 'ADD_CUSTOMER', payload })
+export const addManyCustomersAction = (payload) => ({ type: 'ADD_MANY_CUSTOMERS', payload })
 export const deleteCustomerAction = (payload) => ({ type: 'DELETE_CUSTOMER', payload })
